e258f96e41523fce5e6d5fc9dd00b04739887c3f
Kucky1956 <willy.kuckelsberg@gmail.com>
2025-10-24 16:57:24 +0200

    Main loop and main setup adjusted

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ char msg[MSG_BUFFER_SIZE];
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
-  Serial.print("Message arr   ived [");
+  Serial.print("Message arr  ived [");
   Serial.print(topic);
   Serial.print("] ");
   for (int i = 0; i < length; i++)
