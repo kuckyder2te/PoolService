91b76f7ecba922fc63327fa67918a7b5a99ba557
Willy <willy.kuckelsberg@gmail.com>
2025-11-02 12:29:34 +0100

    not complete -> need adjustments

--- include/Services/pumpError.h
+++ include/Services/pumpError.h
@@ -1,7 +1,7 @@
 #pragma once
 /// @cond
 #include <Arduino.h>
-#define LOCAL_DEBUG
+//#define LOCAL_DEBUG
 #include "myLogger.h"
 /// @endcond
 
@@ -17,12 +17,12 @@ namespace Services
     {
         // PubSubClient *_client;
         // char msg[30];
-        uint8_t _pin_algizid;
-        uint8_t _pin_algizid_mon; // Is that necessary?  Kucky
-        uint8_t _pin_naoh;
-        uint8_t _pin_naoh_mon;
-        uint8_t _pin_hcl;
-        uint8_t _pin_hcl_mon;
+        // uint8_t _pin_algizid;
+        // uint8_t _pin_algizid_mon; // Is that necessary?  Kucky
+        // uint8_t _pin_naoh;
+        // uint8_t _pin_naoh_mon;
+        // uint8_t _pin_hcl;
+        // uint8_t _pin_hcl_mon;
 
     private:
         struct PumpInfo
@@ -33,6 +33,7 @@ namespace Services
             bool lastError = false;
             uint8_t stableCount = 0;
             bool lastMonitorState = false;
+            unsigned long pumpOnSince = 0; // für Timeout-Erkennung
         };
 
         static const uint8_t NUM_PUMPS = 3;
@@ -40,6 +41,7 @@ namespace Services
 
         // wie viele aufeinanderfolgende gleiche Werte nötig sind, um stabil zu sein
         static const uint8_t debounceLimit = 3;
+        static const unsigned long timeoutMs = 10000; // 10 Sekunden Timeout
 
     public:
         pumpError(const String &name)
@@ -47,29 +49,58 @@ namespace Services
         {
         }
 
-        pumpError *init(const uint8_t pin_algizid_mon, const uint8_t pin_algizid,
-                        const uint8_t pin_hcl_mon, const uint8_t pin_hcl,
-                        const uint8_t pin_naoh_mon, const uint8_t pin_naoh)
+        pumpError *init(const uint8_t algizid_mon, const uint8_t algizid_pump,
+                        const uint8_t hcl_mon, const uint8_t hcl_pump,
+                        const uint8_t naoh_mon, const uint8_t naoh_pump)
         {
-            LOGGER_VERBOSE("enter ..."); // Is that necessary?  Kucky
-            _pin_algizid = pin_algizid;
-            _pin_algizid_mon = pin_algizid_mon;
-            _pin_naoh = pin_naoh;
-            _pin_naoh_mon = pin_naoh_mon;
-            _pin_hcl = pin_hcl;
-            _pin_hcl_mon = pin_hcl_mon;
+            LOGGER_VERBOSE("enter pumpError::init ..."); // Is that necessary?  Kucky
+            // _pin_algizid = algizid_pump;
+            // _pin_algizid_mon = algizid_mon;
+            // _pin_naoh = naoh_pump;
+            // _pin_naoh_mon = naoh_mon;
+            // _pin_hcl = hcl_pump;
+            // _pin_hcl_mon = hcl_mon;
 
-            LOGGER_VERBOSE("leave ...");
-            return this;
+            {
+                pumps[0].pumpPin = algizid_pump;
+                pumps[0].monitorPin = algizid_mon;
+                pumps[0].name = "Algizid";
+
+                pumps[1].pumpPin = hcl_pump;
+                pumps[1].monitorPin = hcl_mon;
+                pumps[1].name = "HCl";
+
+                pumps[2].pumpPin = naoh_pump;
+                pumps[2].monitorPin = naoh_mon;
+                pumps[2].name = "NaOH";
+
+                for (uint8_t i = 0; i < NUM_PUMPS; i++)
+                {
+                    pinMode(pumps[i].pumpPin, INPUT);
+                    digitalWrite(pumps[i].pumpPin, LOW); // sicherheitshalber aus
+                    pinMode(pumps[i].monitorPin, INPUT);
+                }
+
+                LOGGER_NOTICE("PumpError task initialized");
+                return this;
+            }
         }
 
         virtual void begin() override
         {
-            LOGGER_NOTICE("Pump errors");
+            LOGGER_NOTICE("Pump errors startet");
         }
 
         virtual void update() override
         {
+            {
+                for (uint8_t i = 0; i < NUM_PUMPS; i++)
+                {
+                    checkPump(pumps[i]);
+                }
+            }
+
+            /*
             static bool algizid_err = false;
             static bool hcl_err = false;
             static bool naoh_err = false;
@@ -105,6 +136,87 @@ namespace Services
             {
                 algizid_err = true; // Malfunktion   must this be?
                 _network->pubMsg("outGarden/hcl_error", "true");
+            }
+                */
+        }
+
+    private:
+        void checkPump(PumpInfo &pump)
+        {
+            bool pumpState = digitalRead(pump.pumpPin);
+            bool monState = digitalRead(pump.monitorPin);
+
+            // --- Debouncing ---
+            if (monState == pump.lastMonitorState)
+            {
+                if (pump.stableCount < debounceLimit)
+                    pump.stableCount++;
+            }
+            else
+            {
+                pump.stableCount = 0;
+                pump.lastMonitorState = monState;
+            }
+
+            // --- Timeout-Überwachung ---
+            if (pumpState == HIGH)
+            {
+                if (pump.pumpOnSince == 0)
+                    pump.pumpOnSince = millis();
+
+                if (monState == LOW && (millis() - pump.pumpOnSince > timeoutMs))
+                {
+                    if (!pump.lastError)
+                    {
+                        pump.lastError = true;
+                        sendStatus(pump, true, "Timeout: no signal from monitor");
+                    }
+                }
+            }
+            else
+            {
+                pump.pumpOnSince = 0; // zurücksetzen, wenn Pumpe aus
+            }
+
+            // --- Normaler Fehlzustand (nach Debounce stabil) ---
+            if (pump.stableCount >= debounceLimit)
+            {
+                bool error = (pumpState == HIGH && monState == LOW);
+
+                if (error != pump.lastError)
+                {
+                    pump.lastError = error;
+                    sendStatus(pump, error);
+                }
+            }
+        }
+
+        void sendStatus(const PumpInfo &pump, bool error, const char *reason = nullptr)
+        {
+            JsonDocument msg;
+            msg["pump"] = pump.name;
+            msg["error"] = error;
+            msg["timestamp"] = millis();
+            if (reason)
+                msg["reason"] = reason;
+
+            String topic = String("outGarden/pump/") + pump.name + "/error";
+            _network->pubMsg(topic.c_str(), msg);
+
+            if (error)
+            {
+                if (reason)
+                {
+                    LOGGER_ERROR_FMT("%s pump error: %s", pump.name, reason);
+                }
+                else
+                {
+                    LOGGER_ERROR_FMT("%s pump error detected!", pump.name);
+                }
+            }
+            else
+            {
+                LOGGER_NOTICE_FMT("%s pump back to normal", pump.name);
             }
         }
     };
