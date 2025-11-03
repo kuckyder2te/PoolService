e22182bcc245812f626363d00733cdf909a0d3de
Kucky1956 <willy.kuckelsberg@gmail.com>
2025-11-03 23:48:10 +0100

    pump monitor PINs correctly named

--- include/Services/pump_algizid.h
+++ include/Services/pump_algizid.h
@@ -4,7 +4,7 @@
 namespace Services {
 class Pump_algizid : public PumpBase {
 public:
-    Pump_algizid(uint8_t pin, uint8_t monitorPin = 255, bool monitorActiveLow = true)
+    Pump_algizid(uint8_t pin, uint8_t monitorPin, bool monitorActiveLow = true)
       : PumpBase(pin, "algizid_pump", 200, 0, monitorPin, monitorActiveLow, 500) {}
 };
 } // namespace Services
--- include/Services/pump_hcl.h
+++ include/Services/pump_hcl.h
@@ -5,7 +5,7 @@ namespace Services {
 class Pump_hcl : public PumpBase {
 public:
     // Optional: monitorPin default 255 = none
-    Pump_hcl(uint8_t pin, uint8_t monitorPin = 255, bool monitorActiveLow = true)
+    Pump_hcl(uint8_t pin, uint8_t monitorPin, bool monitorActiveLow = true)
       : PumpBase(pin, "hcl_pump", 200, 0, monitorPin, monitorActiveLow, 500) {}
 };
 } // namespace Services
