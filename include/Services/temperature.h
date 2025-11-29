8d279c2ba16d421c474f58dd3c622a3ce619dde4
Willy <willy.kuckelsberg@gmail.com>
2025-11-29 12:41:32 +0100

    Temperature Min Max insert

--- include/Services/temperature.h
+++ include/Services/temperature.h
@@ -20,6 +20,8 @@ namespace Services
         DallasTemperature *_sensor;
         char _msg[30];
         float _temperaturePool;
+        float _temperaturePoolMin;
+        float _temperaturePoolMax;
 
     public:
         Temperature(const String &name) : Task::Base(name)
@@ -28,9 +30,10 @@ namespace Services
         }
         virtual void begin() override
         {
-            LOGGER_NOTICE("Dallas Temperature IC Control Library Demo");            
+            LOGGER_NOTICE("Dallas Temperature IC Control Library Demo");
         }
-        Temperature *init(const uint8_t DallasPin){
+        Temperature *init(const uint8_t DallasPin)
+        {
             LOGGER_VERBOSE("enter ...");
             _interface = new OneWire(DallasPin);
             _sensor = new DallasTemperature(_interface);
@@ -41,6 +44,9 @@ namespace Services
 
         virtual void update() override
         {
+            _temperaturePoolMax = _temperaturePool;
+            _temperaturePoolMin = _temperaturePool;
+
             _sensor->requestTemperatures(); // Send the command to get temperatures
             _temperaturePool = _sensor->getTempCByIndex(0);
 
@@ -53,6 +59,21 @@ namespace Services
             {
                 LOGGER_ERROR("Error: Could not read temperature data");
             }
-        }
+
+            if (_temperaturePool >= _temperaturePoolMax)
+            {
+                _temperaturePoolMax = _temperaturePool;
+                sprintf(_msg, "{ \"value\":%.1f }", _temperaturePoolMax);
+                _network->pubMsg("outGarden/temperatureMax", _msg);
+                LOGGER_NOTICE_FMT("Tempearture Max: %,1f", _temperaturePoolMax);
+            }
+            if (_temperaturePool < _temperaturePoolMin)
+            {
+                _temperaturePoolMin = _temperaturePool;
+                sprintf(_msg, "{ \"value\":%.1f }", _temperaturePoolMin);
+                _network->pubMsg("outGarden/temperatureMin", _msg);
+                LOGGER_NOTICE_FMT("Tempearture Min: %,1f", _temperaturePoolMin);
+            }
+        };
     };
 } // End namespace Services
