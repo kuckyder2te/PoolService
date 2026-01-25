ca30bf66febb48318e5c2c8d8ffc0ad7cf6c7503
Willy <willy.kuckelsberg@gmail.com>
2026-01-25 10:55:58 +0100

    feat(ambience): add fade speed control and default mode setting
    - Added FadeSpeed message class for MQTT fade speed control
    - Changed default light mode from OFF to ON (static)
    - Set default fade period to 20 seconds
    - Registered new MQTT message handlers for mode and fadespeed
    - Improved code formatting and structure consistency

--- include/Services/ambience.h
+++ include/Services/ambience.h
@@ -21,20 +21,23 @@ namespace Services
         uint8_t _LED_green_pin;
         uint8_t _LED_blue_pin;
         uint8_t _r, _g, _b;
-        static Ambience* _instance; // Static pointer to the instance
-        
+        uint32_t _fadePeriodMs = 20000; // Default: 20s
+        static Ambience *_instance;     // Static pointer to the instance
+
         // State machine states
-        enum class LightState {
+        enum class LightState
+        {
             OFF,
             ON,
             FADE,
             COLOR_CYCLE,
             BREATHING
         };
-        
+
         LightState _currentState = LightState::OFF;
-        LightState _targetState = LightState::OFF;
-        
+        // LightState _targetState = LightState::OFF;
+        LightState _targetState = LightState::ON; // Default = Static
+
         // For animations
         unsigned long _lastUpdate = 0;
         uint8_t _animationStep = 0;
@@ -54,6 +57,21 @@ namespace Services
             bool call(JsonDocument payload); // Hier der call Prototyp aus der Message Class
         };
 
+        class Mode : public Message
+        {
+        public:
+            Mode(String topic) : Message(topic) {}
+
+            bool call(JsonDocument payload) override;
+        };
+
+        class FadeSpeed : public Message
+        {
+        public:
+            FadeSpeed(String topic) : Message(topic) {}
+            bool call(JsonDocument payload) override;
+        };
+
     private:
         bool _initialized = false;
 
@@ -67,22 +85,27 @@ namespace Services
             _instance = this; // Store the instance pointer
             msgBroker.registerMessage(new State("pool/light/state"));
             msgBroker.registerMessage(new Color("pool/light/colors/rgb"));
+            msgBroker.registerMessage(new Mode("pool/light/mode"));
+            msgBroker.registerMessage(new Mode("pool/light/fadespeed"));
         }
 
         // Method to set the state
-        void setState(LightState newState) {
-            if (_instance != nullptr) {
+        void setState(LightState newState)
+        {
+            if (_instance != nullptr)
+            {
                 _instance->_currentState = newState;
             }
         }
         // Method to set the color
-        void setColor(uint8_t r, uint8_t g, uint8_t b){
+        void setColor(uint8_t r, uint8_t g, uint8_t b)
+        {
             _r = r;
             _g = g;
             _b = b;
         }
 
-        Ambience* init(uint8_t led_red, uint8_t led_green, uint8_t led_blue) 
+        Ambience *init(uint8_t led_red, uint8_t led_green, uint8_t led_blue)
         {
             _LED_red_pin = led_red;
             _LED_green_pin = led_green;
@@ -92,7 +115,7 @@ namespace Services
             pinMode(_LED_red_pin, OUTPUT);
             pinMode(_LED_green_pin, OUTPUT);
             pinMode(_LED_blue_pin, OUTPUT);
-            
+
             // Turn off LEDs initially
             analogWrite(_LED_red_pin, 255);
             analogWrite(_LED_green_pin, 255);
@@ -105,148 +128,208 @@ namespace Services
         virtual void update() override
         {
             // State machine implementation
-            switch (_currentState) {
-                case LightState::OFF:
-                    handleOffState();
-                    break;
-                case LightState::ON:
-                    handleOnState();
-                    break;
-                case LightState::FADE:
-                    handleFadeState();
-                    break;
-                case LightState::COLOR_CYCLE:
-                    handleColorCycleState();
-                    break;
-                case LightState::BREATHING:
-                    handleBreathingState();
-                    break;
+            switch (_currentState)
+            {
+            case LightState::OFF:
+                handleOffState();
+                break;
+            case LightState::ON:
+                handleOnState();
+                break;
+            case LightState::FADE:
+                handleFadeState();
+                break;
+            case LightState::COLOR_CYCLE:
+                handleColorCycleState();
+                break;
+            case LightState::BREATHING:
+                handleBreathingState();
+                break;
             }
         }
 
     private:
-        void handleOffState() {
+        void handleOffState()
+        {
             // Turn off all LEDs
             analogWrite(_LED_red_pin, 255);
             analogWrite(_LED_green_pin, 255);
             analogWrite(_LED_blue_pin, 255);
         }
 
-        void handleOnState() {
+        void handleOnState()
+        {
             // Keep LEDs at current color
             analogWrite(_LED_red_pin, 255 - _r);
             analogWrite(_LED_green_pin, 255 - _g);
             analogWrite(_LED_blue_pin, 255 - _b);
         }
 
-        void handleFadeState() {
+        void handleFadeState()
+        {
             // Fade between colors
             unsigned long currentTime = millis();
-            if (currentTime - _lastUpdate >= 50) { // Update every 50ms
+            if (currentTime - _lastUpdate >= 50)
+            { // Update every 50ms
                 _lastUpdate = currentTime;
-                
+
                 // Simple fade implementation - cycle through colors
                 _animationStep = (_animationStep + 1) % 255;
                 uint8_t value = _animationStep;
-                
+
                 analogWrite(_LED_red_pin, 255 - value);
                 analogWrite(_LED_green_pin, 255 - ((value + 85) % 255));
                 analogWrite(_LED_blue_pin, 255 - ((value + 170) % 255));
             }
         }
 
-        void handleColorCycleState() {
+        void handleColorCycleState()
+        {
             // Cycle through predefined colors
             unsigned long currentTime = millis();
-            if (currentTime - _lastUpdate >= 1000) { // Change color every second
+            if (currentTime - _lastUpdate >= 1000)
+            { // Change color every second
                 _lastUpdate = currentTime;
-                
+
                 // Cycle through colors: Red, Green, Blue, Yellow, Purple, Cyan, White
-                switch (_animationStep % 7) {
-                    case 0: // Red
-                        analogWrite(_LED_red_pin, 0);
-                        analogWrite(_LED_green_pin, 255);
-                        analogWrite(_LED_blue_pin, 255);
-                        break;
-                    case 1: // Green
-                        analogWrite(_LED_red_pin, 255);
-                        analogWrite(_LED_green_pin, 0);
-                        analogWrite(_LED_blue_pin, 255);
-                        break;
-                    case 2: // Blue
-                        analogWrite(_LED_red_pin, 255);
-                        analogWrite(_LED_green_pin, 255);
-                        analogWrite(_LED_blue_pin, 0);
-                        break;
-                    case 3: // Yellow
-                        analogWrite(_LED_red_pin, 0);
-                        analogWrite(_LED_green_pin, 0);
-                        analogWrite(_LED_blue_pin, 255);
-                        break;
-                    case 4: // Purple
-                        analogWrite(_LED_red_pin, 0);
-                        analogWrite(_LED_green_pin, 255);
-                        analogWrite(_LED_blue_pin, 0);
-                        break;
-                    case 5: // Cyan
-                        analogWrite(_LED_red_pin, 255);
-                        analogWrite(_LED_green_pin, 0);
-                        analogWrite(_LED_blue_pin, 0);
-                        break;
-                    case 6: // White
-                        analogWrite(_LED_red_pin, 0);
-                        analogWrite(_LED_green_pin, 0);
-                        analogWrite(_LED_blue_pin, 0);
-                        break;
+                switch (_animationStep % 7)
+                {
+                case 0: // Red
+                    analogWrite(_LED_red_pin, 0);
+                    analogWrite(_LED_green_pin, 255);
+                    analogWrite(_LED_blue_pin, 255);
+                    break;
+                case 1: // Green
+                    analogWrite(_LED_red_pin, 255);
+                    analogWrite(_LED_green_pin, 0);
+                    analogWrite(_LED_blue_pin, 255);
+                    break;
+                case 2: // Blue
+                    analogWrite(_LED_red_pin, 255);
+                    analogWrite(_LED_green_pin, 255);
+                    analogWrite(_LED_blue_pin, 0);
+                    break;
+                case 3: // Yellow
+                    analogWrite(_LED_red_pin, 0);
+                    analogWrite(_LED_green_pin, 0);
+                    analogWrite(_LED_blue_pin, 255);
+                    break;
+                case 4: // Purple
+                    analogWrite(_LED_red_pin, 0);
+                    analogWrite(_LED_green_pin, 255);
+                    analogWrite(_LED_blue_pin, 0);
+                    break;
+                case 5: // Cyan
+                    analogWrite(_LED_red_pin, 255);
+                    analogWrite(_LED_green_pin, 0);
+                    analogWrite(_LED_blue_pin, 0);
+                    break;
+                case 6: // White
+                    analogWrite(_LED_red_pin, 0);
+                    analogWrite(_LED_green_pin, 0);
+                    analogWrite(_LED_blue_pin, 0);
+                    break;
                 }
                 _animationStep++;
             }
         }
 
-        void handleBreathingState() {
+        void handleBreathingState()
+        {
             // Breathing effect (fade in and out)
             unsigned long currentTime = millis();
-            if (currentTime - _lastUpdate >= 30) { // Update every 30ms
+            if (currentTime - _lastUpdate >= 30)
+            { // Update every 30ms
                 _lastUpdate = currentTime;
-                
+
                 // Calculate breathing effect using sine wave approximation
                 float angle = (_animationStep * 3.14159 * 2) / 255;
                 uint8_t brightness = (sin(angle) + 1) * 127.5;
                 _animationStep = (_animationStep + 1) % 255;
-                
+
                 // Apply brightness to current color
                 uint8_t red = (brightness * (255 - _r)) / 255;
                 uint8_t green = (brightness * (255 - _g)) / 255;
                 uint8_t blue = (brightness * (255 - _b)) / 255;
-                
+
                 analogWrite(_LED_red_pin, 255 - red);
                 analogWrite(_LED_green_pin, 255 - green);
                 analogWrite(_LED_blue_pin, 255 - blue);
             }
         }
     };
-    
+
     bool Ambience::State::call(JsonDocument payload)
     { // Hier die call Implementierung des Prototyps
         LOGGER_NOTICE_FMT("Set State to: %d", (uint8_t)payload["value"]);
+
         if ((uint8_t)payload["value"])
         {
-            _instance->setState(LightState::ON);
+            _instance->setState(_instance->_targetState); // Neu ChatGPT
         }
         else
         {
             _instance->setState(LightState::OFF);
         }
-        return _network->pubMsg("pool_light/state", payload);
+        return _network->pubMsg("pool/light/state", payload);
     }
 
     bool Ambience::Color::call(JsonDocument payload)
     {
         LOGGER_NOTICE("Enter");
-        _instance->setColor(payload["value"]["r"],payload["value"]["g"],payload["value"]["b"]);
-        return _network->pubMsg("pool_light/colors/rgb", payload);
+        _instance->setColor(payload["value"]["r"], payload["value"]["g"], payload["value"]["b"]);
+        return _network->pubMsg("pool/light/colors/rgb", payload);
     }
 
-    Ambience* Ambience::_instance = nullptr; // Initialize the static instance pointer
+    bool Ambience::Mode::call(JsonDocument payload)
+    {
+        const uint8_t m = (uint8_t)(payload["value"] | 1);
+        LOGGER_NOTICE_FMT("Mode command received: %d", m);
+    
+        // Mapping:
+        // 1 = Static
+        // 2 = Green <-> Blue (Fade)
+        // 3 = Rainbow
+        LightState target = LightState::ON;
+
+        switch (m)
+        {
+        case 1:
+            target = LightState::ON;
+            break;
+        case 2:
+            target = LightState::FADE;
+            break;
+        case 3:
+            target = LightState::COLOR_CYCLE;
+            break;
+        default:
+            target = LightState::ON;
+            break;
+        }
+
+        _instance->_targetState = target;
+
+        LOGGER_NOTICE_FMT("targetState now: %d", (uint8_t)_instance->_targetState);
+
+        // Wenn Licht nicht OFF ist, sofort umschalten
+        if (_instance->_currentState != LightState::OFF)
+        {
+            _instance->setState(target);
+        }
+
+        return true;
+    }
+
+    bool Ambience::FadeSpeed::call(JsonDocument payload)
+    {
+        uint32_t s = payload["value"] | 20; // z. B. Sekunden
+        LOGGER_NOTICE_FMT("FadeSpeed set to %u", s);
+
+        _instance->_fadePeriodMs = s * 1000;
+        return true;
+    }
+
+    Ambience *Ambience::_instance = nullptr; // Initialize the static instance pointer
 
 } // End namespace Services
